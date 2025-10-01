d06de159478e23268ddf0d364505663860e8d5d5
bamiodunlami18 <bamiodunlami18@gmail.com>
2025-10-01 19:27:12 +0100

    logout added

--- users/user.auth.js
+++ users/user.auth.js
@@ -2,11 +2,16 @@ const path = require('path');
 
 const passport = require(path.join(__dirname, '..', 'config', 'passport'));
 
-
-const loginAuth =  passport.authenticate('local', {
+const loginAuth = passport.authenticate('local', {
   successRedirect: '/todo',
   failureRedirect: '/',
-  failureFlash: true
+  failureFlash: true,
 });
 
-module.exports = {loginAuth};
+const logout = (req, res) => {
+  req.logout((err, done) => {
+    !err ? res.redirect('/') : console.log(err);
+  });
+};
+
+module.exports = { loginAuth, logout };
--- users/users.router.js
+++ users/users.router.js
@@ -5,11 +5,12 @@ const path = require('path');
 const userController = require(path.join(__dirname, 'users.controller'));
 const userAuth = require(path.join(__dirname, 'user.auth'));
 
-router.
-get('/', userController.renderHome)
-.post('/login', userAuth.loginAuth)
+router
+  .get('/', userController.renderHome)
+  .post('/login', userAuth.loginAuth)
 
-.get('/register', userController.renderRegister)
-.post('/register', userController.registerUser)
+  .get('/register', userController.renderRegister)
+  .post('/register', userController.registerUser)
+  .get('/logout', userAuth.logout);
 
 module.exports = router;
